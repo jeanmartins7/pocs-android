25ce39124d6070d14c48778ed7b2a452beebcebb
jeanmartins7 <jeanmarcel8@gmail.com>
2022-04-14 11:32:15 -0300

    projeto listView

---
+++ poc-listView-adapter-custom/app/src/main/java/br/edu/utfpr/martins/listviewcustomadapter/Exercicio.java
@@ -0,0 +1,44 @@
+package br.edu.utfpr.martins.listviewcustomadapter;
+
+public class Exercicio {
+
+    private String exercicio;
+    private String nRepeticoes;
+    private String nSeries;
+    private String peso;
+    private String repouso;
+    private int exercicioImageId;
+
+    public Exercicio (String exercicio, String nRepeticoes, String nSeries, String peso, String repouso, int img){
+        this.exercicio = exercicio;
+        this.nRepeticoes = nRepeticoes;
+        this.nSeries = nSeries;
+        this.peso = peso;
+        this.repouso = repouso;
+        this.exercicioImageId = img;
+    }
+
+    public String getExercicio() {
+        return exercicio;
+    }
+
+    public String getnRepeticoes() {
+        return nRepeticoes;
+    }
+
+    public String getnSeries() {
+        return nSeries;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public String getRepouso() {
+        return repouso;
+    }
+
+    public int getExercicioImageId() {
+        return exercicioImageId;
+    }
+}
